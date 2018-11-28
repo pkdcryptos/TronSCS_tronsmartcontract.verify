835d525b63686cd16e6833476131b6c836fa9f29
tronsmartcontractspacebot <botgithub@tronsmartcontract.space>
2018-11-28 20:25:45 +0700

    Verify T9zDgdXHTsmaPuDRHEufaafSPN73ykzXwN

--- testnet/T9zDgdXHTsmaPuDRHEufaafSPN73ykzXwN/source.sol
+++ testnet/T9zDgdXHTsmaPuDRHEufaafSPN73ykzXwN/source.sol
@@ -8,7 +8,19 @@ contract SafeMath {
   /**
   * @dev Multiplies two numbers, reverts on overflow.
   */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+  
+
+  /**
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
     // benefit is lost if 'b' is also tested.
     // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
@@ -21,18 +33,6 @@ contract SafeMath {
 
     return c;
   }
-
-  /**
-  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b > 0); // Solidity only automatically asserts when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-    return c;
-  }
-
   /**
   * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
   */
